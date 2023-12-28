ca76579ac0d427b3d0d63c17e1ddd3ef8f966a61
ImanGholami <imangholamiimi@gmail.com>
2023-12-28 16:07:19 +0330

    UPDATE | SETTINGS

--- drf-10-05/config/settings.py
+++ drf-10-05/config/settings.py
@@ -127,3 +127,12 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.MyUser'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+
+    ),
+
+}
