eda4a2e5917e86e8ca9c05b66ce555340e9207d5
ImanGholami <imangholamiimi@gmail.com>
2023-12-31 19:37:27 +0330

    ADD | URLS

--- drf-10-05/accounts/urls.py
+++ drf-10-05/accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import user_list
+from .views import user_list, UserLoginView, UserRegistrationView
 
 urlpatterns = [
     path('user/', user_list, name='user'),
+    path('user-login/', UserLoginView.as_view(), name="login"),
+    path('user-registration/', UserRegistrationView.as_view(), name='register'),
 ]
