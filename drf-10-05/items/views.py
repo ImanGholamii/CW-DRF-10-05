5c692c533781b39e9e443deb98eaf0da3ecc5be7
ImanGholami <imangholamiimi@gmail.com>
2023-12-31 17:35:20 +0330

    UPDATE | VIEWS | TOKEN

--- drf-10-05/items/views.py
+++ drf-10-05/items/views.py
@@ -10,12 +10,13 @@ from rest_framework.permissions import IsAuthenticated
 
 
 class ItemViewSet(ModelViewSet):
+    permission_classes = (IsAuthenticated,)
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
 
 
 class GetItemsAPIView(APIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated,]
 
     def get(self, request):
         item = Item.objects.all()
@@ -26,52 +27,50 @@ class GetItemsAPIView(APIView):
         responses={201: 'Created', 400: 'Bad Request'}
     )
     def post(self, request):
-        if request.user.is_authenticated:
-            serializer = ItemSerializer(data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer = ItemSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class UpdateItemApiView(APIView):
+    permission_classes = (IsAuthenticated,)
     @swagger_auto_schema(
         request_body=ItemSerializer,
         responses={200: 'OKAY', 400: 'BAD REQUEST'}
     )
     def put(self, request, pk, *args, **kwargs):
-        if request.user.is_authenticated:
-            instance = Item.objects.get(id=pk)
-            serializer = ItemSerializer(instance=instance, data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        instance = Item.objects.get(id=pk)
+        serializer = ItemSerializer(instance=instance, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     @swagger_auto_schema(
         request_body=ItemSerializer,
         responses={200: 'OKAY', 400: 'BAD REQUEST'}
     )
     def patch(self, request, pk, *args, **kwargs):
-        if request.user.is_authenticated:
-            instance = Item.objects.get(id=pk)
-            serializer = ItemSerializer(instance=instance, data=request.data, partial=True)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        instance = Item.objects.get(id=pk)
+        serializer = ItemSerializer(instance=instance, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class DeleteItemApiView(APIView):
+    permission_classes = (IsAuthenticated,)
     @swagger_auto_schema(
         request_body=ItemSerializer,
         responses={204: 'No Content', 404: 'Not Found'},
     )
     def delete(self, request, pk, *args, **kwargs):
-        if request.user.is_authenticated:
-            try:
-                instance = Item.objects.get(id=pk)
-            except Item.DoesNotExist:
-                return Response(status=status.HTTP_404_NOT_FOUND)
-            instance.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
+        try:
+            instance = Item.objects.get(id=pk)
+        except Item.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        instance.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
